691a70549179caf74510802d34a1d06e611afbfd
jeonghun87 <tliera1987@gmail.com>
2024-02-20 16:26:04 +0900

    Revert "20240206_11:40"
    This reverts commit 148853fafb079db1e610f155c7465404040965cd.

--- action.py
+++ action.py
@@ -1,6 +1,7 @@
 import bpy
 import json
 
+<<<<<<< HEAD
 #쉐이프 키 애니메이션을 액션 키 애니메이션으로 변환
 
 # 파일 경로
@@ -8,18 +9,23 @@ import json
 # b = 쉐입키 애니메이션 파일
 a_file_path = '/Users/hun/GoodGangLabs Dropbox/Individuals/Jeonghun/project/kikiz/3d/blender/json/kz_avatar_kikiz_driver_00.json'  # 예시 경로, 실제 경로로 변경 필요
 b_file_path = '/Users/hun/GoodGangLabs Dropbox/Individuals/Jeonghun/project/kikiz/3d/blender/json/kz_avatar_kikiz_facecontroller_script_02_shape_keys.json'  # 예시 경로, 실제 경로로 변경 필요
+=======
+# a 파일과 b 파일 로드
+a_file_path = '/Users/hun/GoodGangLabs Dropbox/Individuals/Jeonghun/rnd/PoC/blender_drive/json/drivers_04.json'
+b_file_path = '/Users/hun/GoodGangLabs Dropbox/Individuals/Jeonghun/rnd/PoC/blender_drive/json/poc_shapekey_01_shape_keys.json'
+>>>>>>> parent of 148853f (20240206_11:40)
 
-# 파일 로드
-with open(a_file_path, 'r') as file:
-    a_data = json.load(file)
+with open(a_file_path, 'r') as f:
+    a_data = json.load(f)
 
-with open(b_file_path, 'r') as file:
-    b_data = json.load(file)
+with open(b_file_path, 'r') as f:
+    b_data = json.load(f)
 
-# b 파일의 최대 프레임 값 계산
-max_frame_value = max(len(values) for values in b_data.values())
-print(f"애니메이션을 적용할 최대 프레임 값: {max_frame_value}")
+# a 파일에서 필요한 정보 추출
+shape_key_name = a_data['data_path'].split('["')[1].split('"]')[0]  # "test_up"
+object_name = a_data['variables'][0]['target_id_name']  # "con"
 
+<<<<<<< HEAD
 # 여러 오브젝트에 대한 설정 반영
 for driver in a_data:  # a_data는 드라이버 설정 리스트로 가정
     object_name = driver['variables'][0]['target_id_name']
@@ -65,3 +71,18 @@ for driver in a_data:  # a_data는 드라이버 설정 리스트로 가정
     #     print(f"애니메이션 적용 완료: {object_name}")
     # else:
     #     print(f"오브젝트 {object_name}를 찾을 수 없습니다.")
+=======
+# b 파일에서 쉐입키 애니메이션 값 로드
+animation_values = b_data[shape_key_name]
+
+# 오브젝트 및 쉐입키 찾기
+obj = bpy.data.objects[object_name]
+shape_key = obj.data.shape_keys.key_blocks[shape_key_name]
+
+# 애니메이션 값 적용
+for frame_number, value in enumerate(animation_values):
+    shape_key.value = value
+    shape_key.keyframe_insert(data_path="value", frame=frame_number + 1)  # 프레임 번호는 1부터 시작
+
+print(f"애니메이션 적용 완료: {shape_key_name} on {object_name}")
+>>>>>>> parent of 148853f (20240206_11:40)
